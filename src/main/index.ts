f8ea17fa70b7ae4db83a301b1c7031f5e54ae1bf
Kazuhito Hokamura <k.hokamura@gmail.com>
2022-01-28 21:57:49 +0900

    Make port changable

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,10 +7,15 @@ import { initMenu } from "./menu";
 app.on("ready", async () => {
   initMenu();
 
-  await prepareNext({
-    development: join(__dirname, "../../src/renderer"),
-    production: join(__dirname, "../renderer"),
-  });
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+
+  await prepareNext(
+    {
+      development: join(__dirname, "../../src/renderer"),
+      production: join(__dirname, "../renderer"),
+    },
+    port
+  );
 
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -22,7 +27,7 @@ app.on("ready", async () => {
     },
   });
 
-  const url = isDev ? "http://localhost:8000/" : `file://${join(__dirname, "../renderer/out/index.html")}`;
+  const url = isDev ? `http://localhost:${port}/` : `file://${join(__dirname, "../renderer/out/index.html")}`;
 
   mainWindow.loadURL(url);
 });
